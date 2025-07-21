3d9b6cf32fb11fab8617e95e9e52b390dba3b1ad
sbrinkley <brinkley.e@northeastern.edu>
2025-07-21 15:22:42 -0400

    Implemented add delivery, show user deliveries, and filtering.

--- Migrations/20250721143303_InitialCreate.Designer.cs
+++ Migrations/20250721143303_InitialCreate.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DelivAssist.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250718143247_userdto")]
-    partial class userdto
+    [Migration("20250721143303_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -44,10 +44,6 @@ namespace DelivAssist.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Restaurant")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("RestaurantStreetAddress")
                         .IsRequired()
                         .HasColumnType("TEXT");
 
@@ -94,7 +90,22 @@ namespace DelivAssist.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Shift");
+                    b.ToTable("Shifts");
+                });
+
+            modelBuilder.Entity("DelivAssist.Models.ShiftDelivery", b =>
+                {
+                    b.Property<int>("ShiftId")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("DeliveryId")
+                        .HasColumnType("INTEGER");
+
+                    b.HasKey("ShiftId", "DeliveryId");
+
+                    b.HasIndex("DeliveryId");
+
+                    b.ToTable("ShiftDeliveries");
                 });
 
             modelBuilder.Entity("DelivAssist.Models.User", b =>
@@ -102,6 +113,18 @@ namespace DelivAssist.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
+
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("FirstName")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
 
                     b.Property<string>("Password")
                         .IsRequired()
@@ -170,6 +193,25 @@ namespace DelivAssist.Migrations
                     b.ToTable("UserShifts");
                 });
 
+            modelBuilder.Entity("DelivAssist.Models.ShiftDelivery", b =>
+                {
+                    b.HasOne("DelivAssist.Models.Delivery", "Delivery")
+                        .WithMany("ShiftDeliveries")
+                        .HasForeignKey("DeliveryId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("DelivAssist.Models.Shift", "Shift")
+                        .WithMany("ShiftDeliveries")
+                        .HasForeignKey("ShiftId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Delivery");
+
+                    b.Navigation("Shift");
+                });
+
             modelBuilder.Entity("DelivAssist.Models.UserDelivery", b =>
                 {
                     b.HasOne("DelivAssist.Models.Delivery", "Delivery")
@@ -229,6 +271,8 @@ namespace DelivAssist.Migrations
 
             modelBuilder.Entity("DelivAssist.Models.Delivery", b =>
                 {
+                    b.Navigation("ShiftDeliveries");
+
                     b.Navigation("UserDeliveries");
                 });
 
@@ -239,6 +283,8 @@ namespace DelivAssist.Migrations
 
             modelBuilder.Entity("DelivAssist.Models.Shift", b =>
                 {
+                    b.Navigation("ShiftDeliveries");
+
                     b.Navigation("UserShifts");
                 });
 
